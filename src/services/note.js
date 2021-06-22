56d14a82c5829c4a98faed20232646df8d209a20
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-06-22 17:06:12 -0300

    feat: edit note service

--- src/services/note.js
+++ src/services/note.js
@@ -1,10 +1,11 @@
-const NoteError = require('../errors/noteError');
+const ClientError = require('../errors/clientError');
+const NotFoundError = require('../errors/notFound');
 const UserNotes = require('../database/models/notes');
 const ServerError = require('../errors/serverError');
 
 class NoteService {
   static createNote({ title, content, userId }) {
-    if (!content) throw new NoteError('Preencha a nota');
+    if (!content) throw new ClientError('Preencha a nota');
 
     return UserNotes.create({
       title,
@@ -12,6 +13,25 @@ class NoteService {
       userId,
     }).catch(() => {
       throw new ServerError('Erro ao criar notas');
+    });
+  }
+
+  static editNote({
+    noteId,
+    title,
+    content,
+  }) {
+    const upadtedNote = {};
+    if (title) upadtedNote.title = title;
+    if (content) upadtedNote.content = content;
+
+    return UserNotes.findByPk(noteId).then((note) => {
+      if (!note) throw new NotFoundError('Nota não encontrada');
+
+      return note.update(upadtedNote).then((result) => result.toJSON());
+    }).catch((error) => {
+      if (!error.httpCode) throw new ServerError('Ocorreu um erro ao Atualizar a nota');
+      throw error;
     });
   }
 }
