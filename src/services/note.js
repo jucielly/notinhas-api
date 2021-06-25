430f86fa51f23d0948ab194b773625bcb3e94ac1
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-06-25 13:48:12 -0300

    feat: delete note service created

--- src/services/note.js
+++ src/services/note.js
@@ -1,11 +1,11 @@
-const ClientError = require('../errors/clientError');
+const NoteError = require('../errors/clientError');
 const NotFoundError = require('../errors/notFound');
 const UserNotes = require('../database/models/notes');
 const ServerError = require('../errors/serverError');
 
 class NoteService {
   static createNote({ title, content, userId }) {
-    if (!content) throw new ClientError('Preencha a nota');
+    if (!content) throw new NoteError('Preencha a nota');
 
     return UserNotes.create({
       title,
@@ -32,6 +32,18 @@ class NoteService {
     }).catch((error) => {
       if (!error.httpCode) throw new ServerError('Ocorreu um erro ao Atualizar a nota');
       throw error;
+    });
+  }
+
+  static deleteNote(noteId) {
+    if (!noteId) throw new NotFoundError('Nota não encontrada');
+    return UserNotes.findOne({
+      where: {
+        id: +noteId,
+      },
+    }).then((note) => {
+      if (!note) throw new NotFoundError('Nota não encontrada');
+      return note.destroy();
     });
   }
 }
